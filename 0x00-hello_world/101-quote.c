b65fa3087b7916d2890114cf471de87bf328fd7f
k0ns0l <k0ns0l@proton.me>
2023-03-16 08:23:12 -0700

    Create file; output to stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * The write() function takes three arguments:
+ * 1. The file descriptor- 0|1|2
+ * 2. The message or data to be written
+ * 3. The number of bytes to be written
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (0);
+}
