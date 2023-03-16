f6eb8192ac5878f79dd8d32721940e0f3a00b04f
M. Kali <k0ns0l@proton.me>
2023-03-16 05:49:21 -0700

    Create a simple C file; for debugging

---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Simple program to print the classic "Hello world!" :)
+ *
+ * I created this to serve as a test-file for the shell scripts within this directory.
+ * ACTION::	export CFILE=main.c
+ */
+
+int main()
+{
+	printf("Hello, world!\n");
+}
