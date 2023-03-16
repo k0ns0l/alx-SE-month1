1c1c9c72144134dda17073af644b3fb98a3b3c3e
k0ns0l <k0ns0l@proton.me>
2023-03-16 06:52:27 -0700

    puts()

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Print hardcoded string using only puts() function.
+ * Program `return 0;` indicates successfull execution.
+ */
+int main()
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return 0;
+}
