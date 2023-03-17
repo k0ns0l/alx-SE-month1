d3173b68966f970522559b85339a3966ccf6fcef
k0ns0l <k0ns0l@proton.me>
2023-03-17 13:26:36 -0700

    Add C file to print possible combination of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: integer 0 on success
+ */
+
+int main(void)
+{
+	int a, b;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = '0'; b <= '9'; b++)
+		{
+			if (a != b && a < b)
+			{
+				putchar(a);
+				putchar(b);
+				if (a != '8' || (a == '8' && b != '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
