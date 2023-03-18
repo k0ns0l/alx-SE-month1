b515f0c18c8da6f98f7b7d9aa58e07b549fd0cef
M. Kali <k0ns0l@protonmail.com>
2023-03-17 22:26:06 -0700

    Update program documentation

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,6 +3,19 @@
 /**
  * main - Entry point
  *
+ * This program prints all possible combinations of two-digit numbers,
+ * where the first digit is less than the second digit, and no two digits
+ * are the same. The program accomplishes this by using nested loops to iterate
+ * through all possible pairs of digits, and checking whether they satisfy the
+ * above conditions before printing them to the console.
+ *
+ * In addition, the program pads single-digit numbers with a leading '0',
+ * so that all numbers have two digits.
+ *
+ * The program uses the putchar() function to output characters to the console,
+ * and limits its usage to a maximum of eight times.
+ *
+ *
  * Return: 0 (Success)
  */
 
