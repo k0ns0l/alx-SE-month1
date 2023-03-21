d7d1bd702889b1b53f4af2f5b1c97e6c0cb9eaa4
M. Kali <k0ns0l@protonmail.com>
2023-03-21 14:43:06 -0700

    Fix: multiple definitions of the '_putchar' function

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <string.h>
+
 /**
  * main - Entry point
  * A simple program to print '_putchar'
@@ -9,17 +9,15 @@
 
 int main(void)
 {
-	char str[] = "_putchar";
 
-	int i;
-
-	int len = strlen(str);
-
-	for (i = 0; i < len; i++)
-	{
-		_putchar(str[i]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');	
+	_putchar('r');
 	_putchar('\n');
-
 	return (0);
 }
