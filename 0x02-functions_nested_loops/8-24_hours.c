8b90577bc50ef716ffef4963645754b4cd026ddf
M. Kali <k0ns0l@protonmail.com>
2023-03-22 00:58:42 -0700

    Create: print every [min:sec] of the day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * jack_bauer - function to print every seconds of the day - [00:00 - 23:59]
+ */
+
+void jack_bauer(void)
+{
+	int i = 0;
+
+	while (i < 24)
+	{
+		int j = 0;
+
+		while (j < 60)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
+			j++;
+		}
+		i++;
+	}
+}
