58cf5897bd46681cfddae3ad0df187af3a2e2d27
M. Kali <k0ns0l@protonmail.com>
2023-03-22 12:44:38 -0700

    Create: print sum of two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - adds two integer values.
+ *
+ * Description: A simple funcion which returns the sum of two integers.
+ *
+ * @n: First <int> type argument...
+ * @m: Second <int> type argument...
+ *
+ * Return: (n + m).
+ */
+
+int add(int n, int m)
+{
+	return (n + m);
+}
