e42d3d33d9f211c91f7814b7ffc0209bc5275688
M. Kali <k0ns0l@protonmail.com>
2023-03-22 20:19:34 -0700

    Create: print (nth) times table

---
+++ 0x02-functions_nested_loops/HEADER_FILES/100-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+int main(void)
+{
+	print_times_table(3);
+	_putchar('\n');
+	print_times_table(3);
+	_putchar('\n');
+	print_times_table(3);
+	_putchar('\n');
+	print_times_table(3);
+	return (0);
+}
