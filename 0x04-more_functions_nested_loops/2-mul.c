94f2c2e1be1d901ec8bb69363ba36f5b68c2e9f5
M. Kali <k0ns0l@protonmail.com>
2023-03-23 20:58:26 -0700

    Create: print product of two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiply two integers, return value
+ *
+ * @a: first integer parameter
+ * @b: second integer parameter
+ *
+ * Return: PRODUCT(@a.@b)
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
