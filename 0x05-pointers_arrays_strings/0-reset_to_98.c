aed5a4caee2c4f35d8b4721d8a28b9f10c074e91
M. Kali <k0ns0l@protonmail.com>
2023-03-27 18:17:01 -0700

    Add: Function prototypes - [MAIN_H](https://github.com/k0ns0l/alx-low_level_programming/blob/main/0x05-pointers_arrays_strings/TESTFILE/main.h)

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - update the value of a pointer to 98.
+ *
+ * @n: Pointer parameter of <int> type.
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
