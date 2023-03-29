39632171e038e7efcf347074759d62b541f124fd
M. Kali <k0ns0l@protonmail.com>
2023-03-28 19:08:51 -0700

    Update

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,9 +11,9 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\n'; i++)
+	for (i = 0; str[i] != 0; i++)
 	{
-		if (i % 2 == 0)
+		if ((i % 2) == 0)
 			_putchar(str[i]);
 	}
 	_putchar('\n');
