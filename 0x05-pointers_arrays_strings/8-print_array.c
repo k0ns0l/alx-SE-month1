f8c52777c10734e3dbd497dfe45253f7ec556d06
M. Kali <k0ns0l@protonmail.com>
2023-03-28 17:29:28 -0700

    Create: string manipulation

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_array - prints n elements of an array of integers,
+ * followed by a new line.
+ *
+ * @a: array param.
+ * @n: no. of elements of arg-@a to be printed.
+ *
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < (n - 1); i++)
+	{
+		printf("%d, ", a[i]);
+	}
+		if (i == (n - 1))
+		{
+			printf("%d", a[n - 1]);
+		}
+
+		printf("\n");
+}
