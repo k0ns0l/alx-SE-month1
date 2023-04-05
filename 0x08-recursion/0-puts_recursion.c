7897e9dd1737bbbf070c9f561d740bad05b1455a
M. Kali <k0ns0l@protonmail.com>
2023-04-05 16:06:58 -0700

    Add: recursively prints string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - function that prints a string,
+ * followed by a new line.
+ * @s: <ptr> to string parameter.
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		_putchar('\n');
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
