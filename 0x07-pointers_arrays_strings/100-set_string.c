68f194d9e9c1dc30ddec4440b9148812e4c612b9
M. Kali <k0ns0l@protonmail.com>
2023-04-04 20:58:19 -0700

    Add: modify value to a <ptr>

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - set to a value of a ptr
+ * @s: ptr, to a ptr
+ * @to: updated value, to ptr
+ * Return: void
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
