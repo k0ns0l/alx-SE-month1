b881e1ef4c77309756e66a1071c96d78df4559c7
k0ns0l <k0ns0l@protonmail.com>
2023-04-06 15:48:11 -0700

    Add: pow() prototype

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _pow_recursion -  function that returns the value
+ * of x raised to the power of y.
+ * @x: base parameter
+ * @y: power
+ *
+ * Return: <int> x^y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
