01d0330927eb6f0486b5207e01abefa7827bd05d
k0ns0l <k0ns0l@protonmail.com>
2023-04-06 17:05:34 -0700

    Add: isPrime()

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * is_prime_number - finds prime of a number
+ * @n: integer num
+ * Return: 0, otherwise 1
+ */
+
+int is_prime_number(int n)
+{
+	if (n < 2)
+		return (0);
+	if (n == 2 || n == 3)
+		return (1);
+	if (n % 2 == 0 || n % 3 == 0)
+		return (0);
+	int factor = 5;
+
+	int inc = 2;
+
+	while (inc * inc <= n)
+	{
+		if (n % inc == 0)
+			return (0);
+		/* only check odd factor */
+		inc += factor;
+
+
+		/* alternate between 2 and 4 */
+		factor = 6 - factor;
+	}
+	return (1);
+}
