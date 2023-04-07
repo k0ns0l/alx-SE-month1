c0e2772ea30211b9425fce944f0d3a2a676e9a08
k0ns0l <k0ns0l@protonmail.com>
2023-04-06 21:34:01 -0700

    Update logic control

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -11,9 +11,9 @@ int wildcmp(char *s1, char *s2)
 {
 	if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
-	else if (*s1 == '\0' && *s2 == '\0')
+	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
-	else if (*s2 == '*')
+	if (*s2 == '*')
 	{
 		if (wildcmp(s1, s2 + 1))
 			return (1);
