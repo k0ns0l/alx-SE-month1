0839c61c6f84cb16ffe79c15700a2eb079fe898d
k0ns0l <k0ns0l@protonmail.com>
2023-04-07 19:46:40 -0700

    Update: main() description

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * main - function which prints arg[0] value
+ * @argc: arg count <int>
+ * @argv: arg values (array)
+ * Return: 0 (success)
+ */
 int main(int argc, char **argv)
 {
 	printf("%s\n", argv[argc - 1]);
