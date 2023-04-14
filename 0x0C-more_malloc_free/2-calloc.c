b11061214a86b4beef75ae97a0ba75cbe23b4190
k0ns0l <k0ns0l@protonmail.com>
2023-04-13 17:34:14 -0700

    add: Allocates mem. using calloc()

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - function that allocates memory for an array, using malloc.
+ * @nmemb: number
+ * @size: bytes to allocate
+ * Return: returns a pointer to the allocated memory (success)
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+}
