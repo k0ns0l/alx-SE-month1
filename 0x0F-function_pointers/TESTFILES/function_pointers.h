919739b3b663fb47e9fdb3e5bcb610ae4720893b
k0ns0l <k0ns0l@protonmail.com>
2023-04-19 15:48:40 -0700

    update: prototypes def

--- 0x0F-function_pointers/TESTFILES/function_pointers.h
+++ 0x0F-function_pointers/TESTFILES/function_pointers.h
@@ -1,5 +1,15 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
+#include <stdlib.h>
+#include <stddef.h>
+
+/* Function PROTOTYPES <!-- MANDATORY TASKS --> */
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+/* Function PROTOTYPES <!-- ADVANCED TASKS --> */
+
 
 #endif /* FUNCTION_POINTERS_H */
