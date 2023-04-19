240462eb16e1a637e65a297fbce8ec9aac6e4db1
k0ns0l <k0ns0l@protonmail.com>
2023-04-18 21:37:30 -0700

    update: logic condition

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,10 +10,9 @@ void print_dog(struct dog *d)
 {
 	if (!d)
 		return;
-
 	if (!d->name)
 		d->name = "(nil)";
-	else if (!d->owner)
+	if (!d->owner)
 		d->owner = "(nil)";
 
 	printf("Name: %s\nAge: %.6f\nOwner: %s\n", d->name, d->age, d->owner);
