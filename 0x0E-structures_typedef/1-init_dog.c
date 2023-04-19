89300b0ca0b547417c677e014170400d8640550e
k0ns0l <k0ns0l@protonmail.com>
2023-04-18 20:54:09 -0700

    fix: typo; refactor code!

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,5 @@
 #include "dog.h"
-#define <stdlib.h>
+#include <stdlib.h>
 
 /**
  * init_dog - Initialize a stuct, dog
@@ -10,10 +10,9 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (!d)
-	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-	}
+	if (!d) /* d == NULL */
+		d = malloc(sizeof(struct dog));
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
