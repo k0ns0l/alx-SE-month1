abf54881518796b33232a1393169e1a830bdbe3d
k0ns0l <k0ns0l@protonmail.com>
2023-04-19 15:52:29 -0700

    chore: update prototype comment

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -4,12 +4,12 @@
 #include <stdlib.h>
 #include <stddef.h>
 
-/* Function PROTOTYPES <!-- MANDATORY TASKS --> */
+/* Function PROTOTYPES - MANDATORY TASKS */
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
-/* Function PROTOTYPES <!-- ADVANCED TASKS --> */
+/* Function PROTOTYPES - ADVANCED TASKS */
 
 
 #endif /* FUNCTION_POINTERS_H */
